ba1be7151a81b0755ea8647bdbceb5b1598817bb
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2025-06-29 19:15:33 +0800

    fix(eslint): 修正奇怪的规则不生效问题

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,42 +1,43 @@
-import { globalIgnores } from "eslint/config";
-import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescript";
-import pluginVue from "eslint-plugin-vue";
-import pluginOxlint from "eslint-plugin-oxlint";
+import { globalIgnores } from 'eslint/config'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+import pluginOxlint from 'eslint-plugin-oxlint'
 // import skipFormatting from "@vue/eslint-config-prettier/skip-formatting";
-import prettierConfig from "@vue/eslint-config-prettier";
-import unusedImports from "eslint-plugin-unused-imports";
+import prettierConfig from '@vue/eslint-config-prettier'
+import unusedImports from 'eslint-plugin-unused-imports'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-import { configureVueProject } from "@vue/eslint-config-typescript";
-configureVueProject({ scriptLangs: ["ts", "tsx"] });
+import { configureVueProject } from '@vue/eslint-config-typescript'
+configureVueProject({ scriptLangs: ['ts', 'tsx'] })
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
   {
-    name: "app/files-to-lint",
-    files: ["**/*.{ts,mts,tsx,vue}"]
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
   },
 
-  globalIgnores(["**/dist/**", "**/dist-ssr/**", "**/coverage/**"]),
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
   {
     plugins: {
-      "unused-imports": unusedImports
-    }
+      'unused-imports': unusedImports,
+    },
   },
-  pluginVue.configs["flat/essential"],
+  pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
 
-  ...pluginOxlint.configs["flat/recommended"],
+  ...pluginOxlint.configs['flat/recommended'],
   // skipFormatting,
   prettierConfig,
   {
     rules: {
-      "prettier/prettier": "warn",
-      "unused-imports/no-unused-imports": "error",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "no-unused-vars": "off",
-      "vue/multi-word-component-names": "off"
-  },
-);
+      'prettier/prettier': 'warn',
+      'unused-imports/no-unused-imports': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-unused-vars': 'off',
+      'vue/multi-word-component-names': 'off',
+    },
+  }
+)
